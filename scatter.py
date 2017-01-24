e2bdaadddaf7cb2fa3c2be1c3fa1d84982dbda22
Sydney Peng <speng@ucvts.org>
2017-01-24 01:00:46 +0000

    FINISH

--- scatter.py
+++ scatter.py
@@ -5,7 +5,8 @@ import csv
 import requests
 import pandas as pd
 from bokeh.charts import Scatter, output_file, save
-    
+from bokeh.models import HoverTool, ColumnDataSource
+
 #Open csv file and create csv writer
 file = open("speed.csv","w")
 csv = csv.writer(file,delimiter = ",")
@@ -45,12 +46,24 @@ file.close()
 #give csv file for bokeh to read
 scdata = pd.read_csv("speed.csv")
 
-#Create a scatter plot using bokeh + import scatter
 #Add a hovering legend that gives the name and dex number and base speed if you hover over a dot
-from bokeh.models import HoverTool
 legend = "hover"
-scatter=Scatter(scdata,title="Gen 4 Pokemon Base Speed", x="dex number",y="speed",xlabel="Pokemon Dex Number", ylabel="Pokemon Base Speed", tools = legend)
+source = ColumnDataSource(data=scdata)
+
+#Create a scatter plot using bokeh
+scatter=Scatter(scdata,title="Gen 4 Pokemon Base Speed", 
+    x="dex number",
+    y="speed",
+    xlabel="Pokemon Dex Number", 
+    ylabel="Pokemon Base Speed", 
+    tools=legend, source=source)
+scatter.circle('dex number', 'speed', "name", size=10, source=source)
+
+#Create hovering legend
+hover = scatter.select(dict(type=HoverTool))
+hover.tooltips= ([("Name", "@{name}"), ("Dex Number", "@{dex number}"), ("Base Speed", "@speed"),])
+
+#Create graph file
 output_file("scatter.html")
 save(scatter)
 print("complete")
-
--- scatter1.py
+++ scatter1.py
@@ -7,36 +7,25 @@ from bokeh.models import HoverTool, ColumnDataSource
 
 #give csv file for bokeh to read
 scdata = pd.read_csv("speed.csv")
+print(list(scdata.columns.values))
 
 #Add a hovering legend that gives the name and dex number and base speed if you hover over a dot
-#Create a scatter plot using bokeh
 legend = "hover"
+source = ColumnDataSource(data=scdata)
 
-source = ColumnDataSource(
-  data = dict(
-      x=scdata['dex number'],
-      y=scdata['speed'],
-      name=scdata['name'],
-     )    
-    )
-    
+#Create a scatter plot using bokeh
 scatter=Scatter(scdata,title="Gen 4 Pokemon Base Speed", 
     x="dex number",
     y="speed",
     xlabel="Pokemon Dex Number", 
     ylabel="Pokemon Base Speed", 
     tools=legend, source=source)
+scatter.circle('dex number', 'speed', "name", size=10, source=source)
 
 #Create hovering legend
 hover = scatter.select(dict(type=HoverTool))
-hover.point_policy = "follow_mouse"
-hover.tooltips=[
-    ("Name", "@name"),
-    ("Dex Number", "@x"),
-    ("Base Speed", "@y"),
-]
+hover.tooltips= ([("Name", "@{name}"), ("Dex Number", "@{dex number}"), ("Base Speed", "@speed"),])
 
-     
 #Create graph file
 output_file("scatter.html")
 save(scatter)
